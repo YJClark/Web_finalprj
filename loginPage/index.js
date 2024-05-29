a356fe2e06b22437423dea655c2e8ac3e8e6b735
YJClark <huang.clark0410@gmail.com>
2024-05-29 16:45:43 +0800

    創login folder整理

--- loginPage/index.js
+++ loginPage/index.js
@@ -53,7 +53,7 @@ function login(checked){
     switch(checked){
         //殺手(紅色)
         case "r":
-            window.location.href = "網頁設計/code_red.html";
+            window.location.href = "../網頁設計/code_red.html";
             break;
         //間諜(紫色)
         case "p":
